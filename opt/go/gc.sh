c282700af50d8fe1724fb73e7985818aa43395af
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-11-21 15:58:30 +0100

    vscode wont work on master as go version format is not parsable for it.

--- opt/go/gc.sh
+++ opt/go/gc.sh
@@ -3,6 +3,23 @@ GO=~/dev/opt/go
 cd $GO
 rm -rf $GO/go-darwin-amd64-bootstrap*
 
+if [ "$1" == "master" ]
+then
+	cd $GO/src/
+
+	git checkout master
+	git branch -D release-branch.custom_master
+
+	git fetch
+	git reset --hard
+
+	git checkout -b release-branch.custom_master
+
+	echo go1.99.99 > VERSION
+	git tag -d go1.99.99
+	git tag -am 'customer tag' go1.99.99
+fi
+
 cd $GO/src/src
 time GOROOT_BOOTSTRAP=~/dev/opt/go/goroot GOOS=darwin GOARCH=amd64 ./bootstrap.bash
 

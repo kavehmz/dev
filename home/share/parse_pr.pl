e90c745cefa07960cab57303d3ccb7fe60a59d7c
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-12-14 15:28:46 +0100

    safer

--- home/share/parse_pr.pl
+++ home/share/parse_pr.pl
@@ -11,7 +11,7 @@ chomp $GIT_TOKEN;
 
 for $i (sort { $a->{updated_at} cmp  $b->{updated_at}} @$c) {
     print $i->{updated_at}, ', ', $i->{html_url}, ', ', $i->{user}->{login}, ', ', $i->{head}->{ref}, "\n";
-    if ($i->{updated_at} lt strftime("%Y-%m-%d", localtime(time-86400*35) ) ) {
+    if ($i->{updated_at} lt strftime("%Y-%m-%d", localtime(time-86400*60) ) ) {
         if ($ARGV[0] eq 'close') {
             print "\n Closing ", $i->{html_url}," \n";
             $url=$i->{url};

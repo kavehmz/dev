43b075cee5df5113b4fe7a7c6be6b664cb9fca30
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2017-06-04 17:04:55 +0200

    simpler gcloud installation

--- docker/provision_cloud.sh
+++ docker/provision_cloud.sh
@@ -5,12 +5,7 @@ then
 	echo "install google cloud sdk"
 	echo "deb http://packages.cloud.google.com/apt cloud-sdk-jessie main" | tee /etc/apt/sources.list.d/google-cloud-sdk.list
 	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-	apt-get update && apt-get install -y google-cloud-sdk
-	go get google.golang.org/appengine/cmd/aedeploy
-	# Not enough RAM to install it through source, so using the gcloud installation
-	sed -i -e 's/true/false/' /usr/lib/google-cloud-sdk/lib/googlecloudsdk/core/config.json
-	yes | gcloud components install kubectl
-	ln -s /usr/lib/google-cloud-sdk/bin/kubectl /usr/bin/kubectl
+	apt-get update && apt-get install -y google-cloud-sdk kubectl
 	kubectl completion bash | tee /etc/bash_completion.d/kubectl
 fi
 

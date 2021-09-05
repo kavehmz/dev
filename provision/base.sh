2c3b2cf13e949b3a475e689f9c9805b84b01be3d
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2021-09-05 18:00:37 +0200

    install go

--- provision/base.sh
+++ provision/base.sh
@@ -8,3 +8,6 @@ apt-get update
 apt-get install -y git unzip apt-transport-https curl wget bzip2 build-essential ack-grep apt-utils htop jq tmate tmux nano
 
 # install go
+curl -L https://golang.org/dl/go1.17.linux-amd64.tar.gz -o /tmp/go1.17.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /tmp/go1.17.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' > ~/.bashrc
